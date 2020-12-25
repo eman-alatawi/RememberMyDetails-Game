c52b6cbf8bae2f56ce56e01b6e0f10210cce6256
eman-e-alatawi <eman.eid.alatawi@gmail.com>
2020-12-25 23:35:23 +0300

    change the logic of first level

---
+++ js/main.js
@@ -0,0 +1,49 @@
+// count Down timer
+
+const startingMinutes = 1;
+let time = startingMinutes * 60; //seconds
+const countDownElement = document.getElementById("countdown");
+
+function updateTimer() {
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    countDownElement.innerHTML = `${minutes}:${seconds}`;
+    time !== 0 ? time-- : time;
+}
+setInterval(updateTimer, 1000)
+
+//levels litral object
+let levels = {
+    level_1: ["Hammer", "Gallon", "Saw", "Ruler"]
+}
+
+let score = 0;
+
+$(document).ready(function () {
+    $(".question_box").hide();
+
+    const timeout = function () {
+
+        $(".level1_page").css({
+            "filter": "blur(8px)",
+            "-webkit-filter": "blur(8px)",
+        })
+         $(".question_box").show();
+        var q = $(".question_box > h2").html("Is there a " + levels.level_1[0] + " in the image?");
+    }
+    setTimeout(timeout, 62000);
+
+
+
+
+
+
+})
+
+
+
+
+
+
+
