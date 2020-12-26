413369b4ff73436470a135f6e30caae6750f8156
eman-e-alatawi <eman.eid.alatawi@gmail.com>
2020-12-26 23:26:08 +0300

    the questions appear after the timer is out and the screen is blur

--- js/main.js
+++ js/main.js
@@ -13,12 +13,14 @@ function updateTimer() {
 }
 setInterval(updateTimer, 1000)
 
-var ques = $(".question_box > h2");
-var answers = $(".answer");
-// console.log(answers);
+const ques = document.getElementById("question");
+const answers = Array.from(document.getElementsByClassName("answer"));
+
+console.log(answers);
 
 let currentQues = {};
-var score = 0 ;
+var score = 0;
+let acceptingAnswers = false;
 let quesCounter = 0;
 let remainingQues = [];
 
@@ -27,92 +29,119 @@ let remainingQues = [];
 let levelQuestions = [
     {
         question: "Is there a Hammer in the image?",
-        playerChoice: "YES",
-        playerChoice: "NO",
-        correctAnswer:1
+        playerChoice1: "YES",
+        playerChoice2: "NO",
+        correctAnswer: 1
     },
     {
         question: "Is there a Comb in the image?",
-        playerChoice: "YES",
-        playerChoice: "NO",
-        correctAnswer:2
+        playerChoice1: "YES",
+        playerChoice2: "NO",
+        correctAnswer: 2
     },
     {
         question: "Is there a Saw in the image?",
-        playerChoice: "YES",
-        playerChoice: "NO",
-        correctAnswer:1
+        playerChoice1: "YES",
+        playerChoice2: "NO",
+        correctAnswer: 1
     },
     {
         question: "Is there a TV in the image?",
-        playerChoice: "YES",
-        playerChoice: "NO",
-        correctAnswer:2
+        playerChoice1: "YES",
+        playerChoice2: "NO",
+        correctAnswer: 2
     },
     {
         question: "Is there a Gallon in the image?",
-        playerChoice: "YES",
-        playerChoice: "NO",
-        correctAnswer:1
+        playerChoice1: "YES",
+        playerChoice2: "NO",
+        correctAnswer: 1
     },
     {
         question: "Is there a Ruler in the image?",
-        playerChoice: "YES",
-        playerChoice: "NO",
-        correctAnswer:1
+        playerChoice1: "YES",
+        playerChoice2: "NO",
+        correctAnswer: 1
     },
     {
         question: "Is there a Car in the image?",
-        playerChoice: "YES",
-        playerChoice: "NO",
-        correctAnswer:2
+        playerChoice1: "YES",
+        playerChoice2: "NO",
+        correctAnswer: 2
     },
     {
         question: "Is there a Chain in the image?",
-        playerChoice: "YES",
-        playerChoice: "NO",
-        correctAnswer:1
+        playerChoice1: "YES",
+        playerChoice2: "NO",
+        correctAnswer: 1
     },
-     
+
 ];
 
 const numberOfAllQues = 8;
 
-let startGame = function(){
+let startGame = function () {
     quesCounter = 0;
     score = 0;
     remainingQues = [...levelQuestions];
-    console.log(remainingQues);
+    // console.log(remainingQues);
     GenerateNewQues();
-    ques.text(currentQues.question);
-    
+    ques.innerText = currentQues.question;
+
 };
 
-let GenerateNewQues = function(){
+let GenerateNewQues = function () {
+
+    if (remainingQues.length === 0 || quesCounter >= numberOfAllQues) {
+        //go to next level 
+        return window.location.assign("/level2.html");
+    }
     quesCounter++;
     //generate random question
     const quesIndex = Math.floor(Math.random() * remainingQues.length);
     currentQues = remainingQues[quesIndex];
+    ques.innerText = currentQues.question;
 
-};
-startGame();
- 
+    answers.forEach(answer => {
+        const choice = answer.dataset["choice"];
+        answer.innerText = currentQues["playerChoice" + choice];
+    });
+    //remove the already used question
+    remainingQues.splice(quesIndex, 1);
+
+    acceptingAnswers = true;
+}
+
+answers.forEach(answer => {
+    answer.addEventListener("click", (e) => {
+        if (!acceptingAnswers) return;
+        acceptingAnswers = false;
+
+        const PlayerselectedChoice = e.target;
+        const selectedAnswer = PlayerselectedChoice.dataset["choice"];
+        console.log(selectedAnswer);
+        GenerateNewQues();
+    });
+});
+
+// startGame();
 
 
-// // $(document).ready(function () {
-//     $(".question_box").hide();
+$(document).ready(function () {
+    $(".question_box").hide();
 
-//     const timeout = function () {
+    const timeout = function () {
 
-//         $(".level1_page").css({
-//             "filter": "blur(8px)",
-//             "-webkit-filter": "blur(8px)",
-//         })
-//         $(".question_box").show();
+        $(".level1_page").css({
+            "filter": "blur(8px)",
+            "-webkit-filter": "blur(8px)",
+        })
+        $(".question_box").show();
 
-//     }
-//     setTimeout(timeout, 62000);
-// // })
+        startGame();
+    }
+    setTimeout(timeout, 62000);
+})
 
 
 
