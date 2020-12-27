a9db87f597e48801ed7f2d710dbe741c1f055308
eman-e-alatawi <eman.eid.alatawi@gmail.com>
2020-12-27 14:14:27 +0300

    add emoji and text based on the score result

--- js/endLevel.js
+++ js/endLevel.js
@@ -2,12 +2,21 @@ const score = localStorage.getItem("theScore");
 const scoreBycorrectAnswer = localStorage.getItem("scoreBycorrectAnswer");
 const numberOfAllQues = localStorage.getItem("numberOfAllQues");
 
-const ques = document.getElementById("question");
+const result = document.getElementById("result");
 
-if(score == scoreBycorrectAnswer * numberOfAllQues){
-    ques.innerText = "WOW, YOU HAVE AN EXCELLENT VISUAL MEMORY!!"
-}else if(score >= (scoreBycorrectAnswer * numberOfAllQues)/2){
-    ques.innerText = "OK, YOU DID PRETTY WELL";
-}else{
-    ques.innerText = "YOU REMEMBER ME, AREN'T YOU?";
-}
+
+
+$(document).ready(function () {
+    //hide all icons except (next level icone)
+    $("i:not(.fas fa-chevron-circle-right)").hide();
+    if (score == scoreBycorrectAnswer * numberOfAllQues) {
+        result.innerText = "WOW, YOU HAVE AN EXCELLENT VISUAL MEMORY!!"
+        $(".fa-grin-stars").show();
+    } else if (score >= (scoreBycorrectAnswer * numberOfAllQues) / 2) {
+        result.innerText = "OK, YOU DID PRETTY WELL";
+        $(".fa-smile-wink").show();
+    } else {
+        result.innerText = "YOU REMEMBER ME, AREN'T YOU?";
+        $(".fa-sad-tear").show();
+    }
+})
