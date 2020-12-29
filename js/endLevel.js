0c24bcccddd7ceec6975c68b5e33139d4d638770
eman-e-alatawi <eman.eid.alatawi@gmail.com>
2020-12-29 14:26:41 +0300

    change the html files to the root directory

--- js/endLevel.js
+++ js/endLevel.js
@@ -26,12 +26,12 @@ var previous_url = document.referrer;
 // console.log("previous url: "+previous_url);
 
 let newLevel = function () {
-    if (previous_url === "http://127.0.0.1:5500/levels/level1.html") {
+    if (previous_url === "http://127.0.0.1:5500/level1.html") {
         window.location.assign("level2.html");
-    } else if (previous_url === "http://127.0.0.1:5500/levels/level2.html") {
+    } else if (previous_url === "http://127.0.0.1:5500/level2.html") {
         window.location.assign("level3.html");
     }
-    else if (previous_url === "http://127.0.0.1:5500/levels/level3.html"){
+    else if (previous_url === "http://127.0.0.1:5500/level3.html"){
         window.location.assign("gameOver.html");
     }
 
